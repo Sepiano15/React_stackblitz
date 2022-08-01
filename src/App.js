2ab2ce0830da5c66a247cc29ff7d99d277b0ee31
Sepiano15 <sepiano15@gmail.com>
2022-08-01 09:04:23 +0900

    Sign In

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function Header() {
   return (
@@ -59,9 +69,51 @@ function Article() {
 export default function App() {
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign In
+          </Typography>
+          <TextField
+            margin="normal"
+            label="Email"
+            required
+            fullWidth
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            required
+            fullWidth
+            name="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel control={<Checkbox />} label="Remember" />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb : 2}}>
+            Sign in
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link>Forgot Password?</Link>
+            </Grid>
+            <Grid item>
+              <Link>Sign Up</Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
     </div>
   );
 }
